c7f80aab2f16eebd1f2b9b7676601aff4c5f2fc5
elderguardian <soydev@riseup.net>
2023-03-31 10:35:45 +0200

    added debug methods

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
 require_once "autoloader.php";
+require_once "SB.php";
 require_once "routes.php";
 
 if (!isset($routes)) {
--- src/controllers/HelloController.php
+++ src/controllers/HelloController.php
@@ -3,6 +3,10 @@
 class HelloController extends Controller {
 
     public function world() {
+        SB::d("debug message");
+        //doSomething()
+        SB::dd("failed with error: 2");
+
         return $this->view('home');
     }
     
